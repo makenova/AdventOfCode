fbbb5e090ad113649c0c227a6afb2df65b440cda
makenova <themarchmyth@gmail.com>
2015-12-22 22:19:33 -0600

    playing with promises

--- readinput.js
+++ readinput.js
@@ -4,20 +4,17 @@ var request = require('request').defaults({jar: true});
 module.exports = readInput;
 
 function readInput(dayNo) {
-  var p = new Promise((resolve, reject)=>{
-    checkCache(dayNo, (data)=>{
-      if (data) {
-        return resolve(data);
-      }
+  return new Promise((resolve, reject)=>{
+    checkCache(dayNo)
+    .then((data) => { return resolve(data); })
+    .catch( (err)=>{
+      console.log(err, 'cache miss');
       var url = `http://adventofcode.com/day/${dayNo}/input`;
       var cookie = request.cookie(`session=${process.env.ADVENTOFCODESESSIONID}`);
       var jar = request.jar();
       jar.setCookie(cookie, url);
 
-      var reqObj = {
-        url: url,
-        jar: jar
-      };
+      var reqObj = { url: url, jar: jar };
 
       request.get(reqObj, (err, res, body) =>{
         if (err) return reject(err);
@@ -27,21 +24,21 @@ function readInput(dayNo) {
         });
       });
     });
-
   });
-  return p;
 }
 
 function checkCache (dayNo, callback) {
-  var filePath = `${__dirname}/cache/${dayNo}`;
-  fs.stat(filePath, (err, stat)=>{
-    if (!err){
-      fs.readFile(filePath, 'utf8', (err, data)=>{
-        return callback(data);
-      });
-    } else {
-      return callback(null); // Intentionally swallow err ... wot!?!
-    }
+  return new Promise((resolve, reject)=>{
+    var filePath = `${__dirname}/cache/${dayNo}`;
+    fs.stat(filePath, (err, stat)=>{
+      if (err) reject(err);
+      else{
+        fs.readFile(filePath, 'utf8', (err, data)=>{
+          if (err) reject(err);
+          else resolve(data);
+        });
+      }
+    });
   });
 }
 
